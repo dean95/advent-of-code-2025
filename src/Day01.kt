930a59ff62f96c64b4109008b185b49742c10702
Dean Djermanovic <dean.djermanovic@five.agency>
2025-12-01 07:54:05 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var res = 0
+        var sum = 50
+
+        input.forEach { line ->
+            val direction = line.first()
+            val num = line.substring(1).toInt()
+
+            when (direction) {
+                'L' -> {
+                    sum = ((sum - num) % 100 + 100) % 100
+                }
+
+                'R' -> {
+                    sum = (sum + num) % 100
+                }
+            }
+
+            if (sum == 0) res++
+        }
+
+        return res
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var res = 0
+        var sum = 50
+
+        input.forEach { line ->
+            val direction = line.first()
+            val num = line.substring(1).toInt()
+
+            when (direction) {
+                'L' -> {
+                    res += Math.floorDiv(sum - 1, 100) - Math.floorDiv(sum - num - 1, 100)
+                    sum = Math.floorMod(sum - num, 100)
+                }
+
+                'R' -> {
+                    res += (sum + num) / 100
+                    sum = (sum + num) % 100
+                }
+            }
+        }
+
+        return res
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
